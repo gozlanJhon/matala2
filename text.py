bfc9336175ea4eadaec1139493735b81d53ef81b
gozlanJhon <80416345+gozlanJhon@users.noreply.github.com>
2021-03-29 13:49:33 +0300

    Add files via upload

---
+++ text.py
@@ -0,0 +1,20 @@
+def revword (word:str)->str:
+    n= len(word)
+    NW=''
+    while n>0:
+        NW+=word[n-1]
+        n-=1
+    NW=NW.rstrip()
+    return NW.lower()
+
+def countword()->int:
+    txt=open('text.txt')
+    txt=txt.read().split()
+    counter=0
+    word=txt[0]
+    for mila in txt:
+        if revword(mila)==word:
+            counter+=1
+    return counter+1
+
+print (countword())
